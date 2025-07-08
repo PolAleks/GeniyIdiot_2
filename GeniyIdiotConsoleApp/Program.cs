4674374505ff042bf274e20957ccfb3457bdf222
PolAleks <aa.polushkin@yandex.ru>
2025-07-08 17:34:29 +0300

    Реализовал метод возвращающий число, с проверкой на корректность ввода

--- GeniyIdiotConsoleApp/Program.cs
+++ GeniyIdiotConsoleApp/Program.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Permissions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GeniyIdiotConsoleApp
 {
