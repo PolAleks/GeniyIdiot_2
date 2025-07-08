79b75c4c4f8b729c149481be7ae141076d99189a
PolAleks <aa.polushkin@yandex.ru>
2025-07-08 20:44:13 +0300

    Реализовал запись результатов тестирования в файл

--- GeniyIdiotConsoleApp/Program.cs
+++ GeniyIdiotConsoleApp/Program.cs
@@ -52,7 +52,7 @@ namespace GeniyIdiotConsoleApp
                 var lines = File.ReadAllLines(file);
                 foreach (var line in lines)
                 {
-                    var item = line.Split(new char['#'], StringSplitOptions.RemoveEmptyEntries);
+                    var item = line.Split(new char[] { '#' }, StringSplitOptions.RemoveEmptyEntries);
                     (string name, int countCorrectAnswer, string diagnosis) = (item[0], Convert.ToInt32(item[1]), item[2]);
                     Console.WriteLine($"{name,-15}{countCorrectAnswer,10}{diagnosis,23}");
                 }
@@ -60,9 +60,9 @@ namespace GeniyIdiotConsoleApp
             else
             {
                 Console.WriteLine("Результаты ранее проведенных тестов отсутствуют!");
+            }
                 Console.WriteLine("Для возвращения в меню, нажмите любую клавишу.");
                 Console.ReadKey();
-            }
         }
 
         static void StartTest()
@@ -89,6 +89,11 @@ namespace GeniyIdiotConsoleApp
                 string diagnosis = GetDiagnosis(countCorrectUserAnswer, answer.Length);
 
                 Console.WriteLine($"{userName} твой диагноз - {diagnosis}");
+
+                var file = Path.Combine(Environment.CurrentDirectory, "log.txt");
+                var result = $"{userName}#{countCorrectUserAnswer}#{diagnosis}";
+                File.AppendAllText(file, result);
+
             } while (Repeat());
         }
 
