30c3afbf7006fe1580d6805491194714cb53829c
PolAleks <aa.polushkin@yandex.ru>
2025-07-10 15:49:23 +0300

    Реализация базовой версии оконной версии игры

--- GeniyIdiotWinFormsApp/MainForm.Designer.cs
+++ GeniyIdiotWinFormsApp/MainForm.Designer.cs
@@ -28,20 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.numberQuestionLabel = new System.Windows.Forms.Label();
+            this.textQuestionLabel = new System.Windows.Forms.Label();
+            this.answerTextBox = new System.Windows.Forms.TextBox();
+            this.nextButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // numberQuestionLabel
+            // 
+            this.numberQuestionLabel.AutoSize = true;
+            this.numberQuestionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberQuestionLabel.Location = new System.Drawing.Point(76, 52);
+            this.numberQuestionLabel.Name = "numberQuestionLabel";
+            this.numberQuestionLabel.Size = new System.Drawing.Size(92, 20);
+            this.numberQuestionLabel.TabIndex = 0;
+            this.numberQuestionLabel.Text = "Вопрос №1";
+            // 
+            // textQuestionLabel
+            // 
+            this.textQuestionLabel.AutoSize = true;
+            this.textQuestionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textQuestionLabel.Location = new System.Drawing.Point(76, 95);
+            this.textQuestionLabel.Name = "textQuestionLabel";
+            this.textQuestionLabel.Size = new System.Drawing.Size(118, 20);
+            this.textQuestionLabel.TabIndex = 1;
+            this.textQuestionLabel.Text = "Текст вопроса";
+            // 
+            // answerTextBox
+            // 
+            this.answerTextBox.Location = new System.Drawing.Point(80, 143);
+            this.answerTextBox.Name = "answerTextBox";
+            this.answerTextBox.Size = new System.Drawing.Size(127, 20);
+            this.answerTextBox.TabIndex = 2;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nextButton.Location = new System.Drawing.Point(80, 194);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(127, 31);
+            this.nextButton.TabIndex = 3;
+            this.nextButton.Text = "Далее";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.answerTextBox);
+            this.Controls.Add(this.textQuestionLabel);
+            this.Controls.Add(this.numberQuestionLabel);
             this.Name = "MainForm";
             this.Text = "Гений Идиот";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label numberQuestionLabel;
+        private System.Windows.Forms.Label textQuestionLabel;
+        private System.Windows.Forms.TextBox answerTextBox;
+        private System.Windows.Forms.Button nextButton;
     }
 }
 
--- GeniyIdiotWinFormsApp/MainForm.cs
+++ GeniyIdiotWinFormsApp/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeniyIdiot.Common;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace GeniyIdiotWinFormsApp
 {
     public partial class MainForm : Form
     {
+        private List<Question> _questions;
+        private int _countQuestions;
+        private User _user;
+        private Question _currentQuestion;
+        private int _numberCurrentQuestion = 1;
         public MainForm()
         {
             InitializeComponent();
+
+            _questions = QuestionsStorage.GetAll();
+            _countQuestions = _questions.Count;
+            _user = new User("Инкогнито");
+
+            GetNextQuestion();
+        }
+
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            var userAnswer = Convert.ToInt32(answerTextBox.Text);
+            if (_currentQuestion.IsAnswerCorrect(userAnswer))
+                _user.AddCorrectAnswer();
+
+            GetNextQuestion();
+        }
+        private void GetNextQuestion()
+        {
+            Random random = new Random();
+            var index = random.Next(_questions.Count);
+
+            _currentQuestion = _questions[index];
+            _questions.Remove(_currentQuestion);
+
+            numberQuestionLabel.Text = $"Вопрос № {_numberCurrentQuestion++}";
+            textQuestionLabel.Text = _currentQuestion.Text;
+
+            if(_questions.Count == 0)
+            {
+                nextButton.Enabled = false;
+                _user.AddDiagnosis(_countQuestions);
+                MessageBox.Show($"{_user.Name} твой диагноз - {_user.Diagnosis}",
+                                "Результат тестирования",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
         }
     }
 }
