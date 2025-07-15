6edb2e45156fa9a1d16d96410f3dd99ce99bb316
PolAleks <aa.polushkin@yandex.ru>
2025-07-15 15:32:06 +0300

    Реализовал универсальный метод для валидации ввода числовых значений TryParseNumber

---
+++ GeniyIdiot.Common/InputValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GeniyIdiot.Common
+{
+    public class InputValidator
+    {
+        public static bool TryParseNumber(string inputData,
+                                   out int value,
+                                   out string errorMessage,
+                                   int minValue = int.MinValue,
+                                   int maxValue = int.MaxValue)
+        {
+            value = default;
+            errorMessage = string.Empty;
+
+            try
+            {
+                value = int.Parse(inputData);
+                if (value < minValue || value > maxValue)
+                    throw new OverflowException();
+                return true;
+            }
+            catch (ArgumentNullException)
+            {
+                errorMessage = "Вы не ввели данные";
+            }
+            catch(FormatException) 
+            {
+                errorMessage = "Ответ должен содержать число!";
+            }
+            catch(OverflowException) 
+            {
+                errorMessage = $"Выбор должен быть в диапазоне от {minValue} до {maxValue}: ";
+            }
+            
+            return false;
+        }
+    }
+}
