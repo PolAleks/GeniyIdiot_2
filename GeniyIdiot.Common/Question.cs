1bb47cc55b6ecf6bb8673b0c59cb8e3ccfa4a283
PolAleks <aa.polushkin@yandex.ru>
2025-07-15 19:50:03 +0300

    Исправил модель Question для коректной работы с сериализацией

--- GeniyIdiot.Common/Question.cs
+++ GeniyIdiot.Common/Question.cs
@@ -2,17 +2,16 @@
 {
     public class Question
     {
-        public int Answer { get ; set; }
         public string Text { get ; set; }
-
+        public int Answer { get ; set; }
         public int Number { get; set; } = 0;
 
+        public Question() { }
         public Question(string text, int answer)
         {
             Text = text;
             Answer = answer;
         }
-        public bool IsAnswerCorrect(int answer) => Answer == answer;
-        public override string ToString() => $"{Text}#{Answer}";
+        public bool IsAnswerCorrect(int answer) => Answer == answer;        
     }
 }
