8e7eff4e6a9563d326caf0f1e40b63e1517bdcdc
PolAleks <aa.polushkin@yandex.ru>
2025-07-15 19:49:07 +0300

    Исправил FileProvider.Load - теперь возвращает текст, вместо строк

--- GeniyIdiot.Common/FileProvider.cs
+++ GeniyIdiot.Common/FileProvider.cs
@@ -8,44 +8,29 @@ namespace GeniyIdiot.Common
     public class FileProvider
     {
         private static string _path = Environment.CurrentDirectory;
-        public static List<string> Load(string file)
+
+        public static string Load(string file)
         {
             file = Path.Combine(_path, file);
-            var content = new List<string>();
-
-            if (File.Exists(file))
-            {
-                using (StreamReader sr = new StreamReader(file, Encoding.Default))
-                {
-                    while (!sr.EndOfStream)
-                    {
-                        content.Add(sr.ReadLine());
-                    }
-                }
-            }
+            
+            string content = File.ReadAllText(file, Encoding.UTF8);
             return content;
         }
 
+        
         public static void Save(string file, string content, bool append = true)
         {
             file = Path.Combine(_path, file);
-            using (StreamWriter sw = new StreamWriter(file, append, Encoding.Default))
+
+            using (StreamWriter sw = new StreamWriter(file, append, Encoding.UTF8))
             {
-                sw.WriteLine(content);
+                sw.Write(content);
             }
         }
 
-        internal static void Delete(string file, string content)
-        {
-            file = Path.Combine(_path, file);
-            var questions = Load(file);
+        
 
-            questions.Remove(content);
-
-            Save(file, string.Join(Environment.NewLine,questions), false);
-        }
-
-        internal static bool Exists(string file)
+        public static bool Exists(string file)
         {
             file = Path.Combine(_path, file);
             return File.Exists(file);
